c63a309da53c9f633c8c437ed580cf6cd18241df
Артур Муниров <munirov.ai@phystech.edu>
2025-03-01 15:18:03 +0300

    Git Check

--- asm.bat
+++ asm.bat
@@ -1,7 +1,7 @@
 @echo off
 cd assembler/src
 g++ -Wall -c AsmFunc.cpp -c AsmMain.cpp
-g++ -Wall -o asm AsmFunc.o AsmMain.o
+g++ -Wall -o asm AsmMain.o AsmFunc.o
 cd ..
 cd ..
 move "assembler\src\asm.exe" ".\" >nul 2>&1
